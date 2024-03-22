05ce341b4f8a13894e892f20415673a5c6862164
Jacob Hildebrandt <jacob.hildebrandt@gmail.com>
2024-03-22 08:48:16 -0700

    adding memory to the agent

--- lang-chain/agents/main.py
+++ lang-chain/agents/main.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
@@ -25,11 +26,14 @@ prompt = ChatPromptTemplate(
             "or what columns exist. Instead, use the 'describe_tables' function"
             )
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
@@ -41,11 +45,19 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 # agent_executor("How many users have provided a shipping address?")
 # agent_executor("How many users have an order?")
 # agent_executor("Whats the highest order value?")
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file. I want the report to include the product name, amount sold and price in a table format.")
+# agent_executor("Summarize the top 5 most popular products. Write the results to a report file. I want the report to include the product name, amount sold and price in a table format.")
+agent_executor(
+    "How many orders are there? Ensure you write the result to an html report and save it to the disk for all future queries."
+)
+
+agent_executor(
+    "Repeat the exact same process for users and products."
+)
 
